bdbf91b4d3acec36dcf78dbe19d267c06f6026f2
atasoya <ataatasoy2013@gmail.com>
2024-09-06 22:20:34 +0300

    Add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-neutral text-neutral-content">
     <div className="flex-1">
         <a className="btn btn-ghost text-xl">emu.tips</a>
     </div>
