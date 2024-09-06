91f713017689ac5e6a79407089000f30991f03fd
atasoya <ataatasoy2013@gmail.com>
2024-09-06 22:34:51 +0300

    Add theme controller

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ThemeController from './ThemeController'
 
 const Navbar = () => {
   return (
@@ -8,16 +9,7 @@ const Navbar = () => {
     </div>
     <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-            <details>
-            <summary>Parent</summary>
-            <ul className="bg-base-100 rounded-t-none p-2">
-                <li><a>Link 1</a></li>
-                <li><a>Link 2</a></li>
-            </ul>
-            </details>
-        </li>
+        <li><ThemeController/></li>
         </ul>
     </div>
     </div>
