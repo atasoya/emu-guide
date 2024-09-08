f8af8ada9a1df9997f74067b2cd8335d75ac5834
atasoya <ataatasoy2013@gmail.com>
2024-09-08 18:53:17 +0300

    Add keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,20 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "emu.guide - Your comprehensive guide to Emu",
+  title: {
+    default: "Home | emu.guide",
+    template: `%s | emu.guide`,
+  },
   description: "Everything you need to know about Eastern Mediterranean University",
+  keywords: ["EMU Guide", "Eastern Mediterranean University", "EMU News", "EMU Cafes", 
+    "EMU Restaurants", "EMU Dormitories", "EMU Health Services", "EMU Fitness", 
+    "EMU Events", "EMU Clubs", "EMU Entertainment", "EMU Faculties", 
+    "EMU Scholarships", "EMU Transportation", "Private Classes in EMU", 
+    "EMU Communities", "EMU Student Guide", "EMU Discounts", "EMU Markets", 
+    "EMU Stores", "EMU Campus Life", "DAÜ Rehberi", "DAÜ Yemekler", 
+    "DAÜ Burslar", "DAÜ Etkinlikler", "DAÜ Ulaşım", "DAÜ Eğlence", 
+    "EMU International Students", "EMU Stationery","DAÜ Özel Dersler","DAÜ Al Sat","DAÜ Topluluklar"
+    ,"DAÜ Rehberler","DAÜ Marketler","DAÜ Taxi"]
 };
 
 export default function RootLayout({
